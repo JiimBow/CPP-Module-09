da5dbe75eceb69ffaa103844d6f9841e59c9d4d3
Jean-Baptiste Odone <jodone@2A4.42angouleme.fr>
2026-06-03 16:10:06 +0200

    add some files

---
+++ ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jodone <jodone@student.42angouleme.fr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/03 13:42:56 by jodone            #+#    #+#             */
+/*   Updated: 2026/06/03 13:50:22 by jodone           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+#include <iostream>
+
+int	main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "You must give positives int as arguments" << std::endl;
+		return 1;
+	}
+
+	
+	return 0;
+}
